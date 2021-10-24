eb984a512598a9200f98df7392a8553ef6db2dcf
NatalyaGavryliuk <natalja.golovko@privatbank.ua>
2021-10-24 10:50:27 +0300

    init commit

--- src/com/pb/gavryliuk/hw3/Array.java
+++ src/com/pb/gavryliuk/hw3/Array.java
@@ -2,7 +2,6 @@ package com.pb.gavryliuk.hw3;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class Array {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
